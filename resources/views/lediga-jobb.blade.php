3875dd2368c41eb11a4f1a858fe761e050bda659
Herman Kullberg <herman.kullberg@hotmail.com>
2017-09-04 19:46:51 +0200

    Added page lediga-job and fixed breadcrumbs

---
+++ resources/views/lediga-jobb.blade.php
@@ -0,0 +1,63 @@
+@extends('master')
+
+@section('content')
+	<section class="bar background-gray no-mb">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="heading text-center">
+                            <h1>Progressive Safety söker Key Account Manager</h1>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <p><strong>Plats:</strong> Jönköping</p>
+                        <p><strong>Yrkesroll:</strong> Key Account Manager </p>
+                        <p><strong>Lön:</strong> Fast lön + provision </p>
+                        <p>
+                            <strong>Anställningsform:</strong> Heltid och tillsvidare med 6 månaders inledande
+                            provanställning.
+                        </p>
+
+                        <p>
+                            Progressive Safety är ett innovationsbolag som utvecklar säkerhets- och
+                            tillgänglighetsprodukter. Vår första produkt, en mobil upphängningsanordning för
+                            brandsläckare, löser ett säkerhetsproblem inom brandskydd och har blivit utsedd till
+                            årets säkerhetslösning 2016.
+                        </p>
+                        <p>
+                            Företaget växer och vi söker nu passionerade säljare med fingertoppskänsla och god
+                            förståelse för kvalitativ ingång och hög nivå i säljprocessen. Du kommer självständigt
+                            driva hela försäljningsprocessen från att identifiera nya affärsmöjligheter/prospektering,
+                            boka möten med potentiella kunder, genomföra kundmöten, offert- och avtalsskrivning och
+                            hela vägen fram till avslut.
+                        </p>
+                        <p>
+                            Du är även ansvarig för uppföljning mot kund efter avslutat uppdrag vilket innebär mycket
+                            dialog och samarbete, både under och efter processen. Vi är ett tillväxtbolag med höga
+                            ambitioner och nykundsbearbetning kommer därför initialt stå för en stor del av arbetstiden.
+                        </p>
+                        <p><strong>Vi tror att du som söker passar in på följande profil:</strong></p>
+                        <ul style="font-size: 18px;">
+                            <li>Du har ett mycket starkt driv och vill vara den bästa på det du gör.</li>
+                            <li>Du är nyfiken och handlingskraftig.</li>
+                            <li>Du har ett stort intresse för människor och är bra på att skapa nya kontakter.</li>
+                            <li>Du är ödmjuk men säker på din förmåga att lösa kundens problem.</li>
+                            <li>Goda kunskaper i Svenska och Engelska.</li>
+                        </ul>
+                        <p>
+                            Det är meriterande om du har en bakgrund inom försäljning sedan tidigare.<br>
+                            Körkort klass B samt inledningsvis tillgång till egen bil är ett krav.
+                        </p>
+                        <h2>Hur ansöker jag?</h2>
+                        <p>
+                            Vill du veta mer är du välkommen att ringa Ahmed Mireé på telefon
+                            <a href="tel:0767055370">076-7055370</a>, Din ansökan skickar du till
+                            <a href="mailto:ahmed.miree@progressivesafety.se">ahmed.miree@progressivesafety.se</a>
+                            så snart som möjligt, dock senast den 30/9-2017. Vi kommer utvärdera ansökningar löpande
+                            och tjänsten kan komma att tillsättas före sista ansökningsdatum.
+                        </p>
+                    </div>
+                </div>
+            </div>
+	</section>
+@endsection

574cd7e468099b3acc4992c2c0dd13fc01bbeb4a
Herman Kullberg <herman.kullberg@hotmail.com>
2017-09-06 13:45:40 +0200

    Added second video to page "så fungerar det"

--- resources/views/how-it-works.blade.php
+++ resources/views/how-it-works.blade.php
@@ -2,28 +2,29 @@
 
 @section('content')
 
-	<section>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<div class="heading text-center">
-						<h2>Så funkar den mobila upphägningsanordningen <b>EasyExtinguish</b></h2>
-						<p style="font-weight:bold;">Här följer en instruktionsvideo på hur brandsläckaren EasyExtinguish fungerar.</p>
-					</div>
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="heading text-center">
+					<h2 class="reset-inline">Så funkar den mobila upphägnings&shy;anordningen <b>EasyExtinguish</b></h2>
 				</div>
 			</div>
 		</div>
-<br>
+	</div>
 
 	<div class="container">
 	    <div class="row">
-	        <div class="col-md-12">
-	            <div class="vid">
-	                <iframe width="560" height="315" src="//www.youtube.com/embed/kzEY6j6c62M" allowfullscreen=""></iframe>
-	            </div><!--./vid -->
 
-	        </div><!--.col -->
+			<div class="col-md-6">
+				<p	style="font-weight:bold;">Presentation av upphängningsanordning EasyExtinguish.</p>
+				<iframe width="100%" height="315" src="//www.youtube.com/embed/kzEY6j6c62M" allowfullscreen=""></iframe>
+			</div>
 
+			<div class="col-md-6 lower-video">
+				<p	style="font-weight:bold;">Instruktionsvideo för montering av EasyExtinguish.</p>
+				<iframe width="100%" height="315" src="https://www.youtube.com/embed/oCa3elbfLaU" frameborder="0" allowfullscreen></iframe>
+			</div>
 	    </div><!--./row -->
 
 	</div><!--./container -->
